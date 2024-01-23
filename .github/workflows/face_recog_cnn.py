f436c11bf5cc7473380bb24b8dc73a6f458eb4bc
Missheba21 <157479072+Missheba21@users.noreply.github.com>
2024-01-23 15:37:07 +0300

    Update face_recog_cnn.py

--- .github/workflows/face_recog_cnn.py
+++ .github/workflows/face_recog_cnn.py
@@ -2,25 +2,13 @@ import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 ​
 import os
-for dirname, _, filenames in os.walk('/kaggle/input'):
+for dirname, _, filenames in os.walk('/github/workflows'):
     for filename in filenames:
         print(os.path.join(dirname, filename))
 
 pip install mtcnn
 
-from matplotlib import pyplot as plt
-import cv2
-from PIL import Image
 
-import mtcnn
-from mtcnn.mtcnn import MTCNN
-from matplotlib.patches import Rectangle
-
-from os import listdir
-from tqdm import tqdm
-import pandas as pd
-from sklearn.model_selection import train_test_split
-import seaborn as sns
 
 
 
