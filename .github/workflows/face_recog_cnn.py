71264c25ec8cd248d15c20f595b35c52b389c89f
Missheba21 <157479072+Missheba21@users.noreply.github.com>
2024-01-23 15:20:17 +0300

    Update face_recog_cnn.py

--- .github/workflows/face_recog_cnn.py
+++ .github/workflows/face_recog_cnn.py
@@ -1,9 +1,26 @@
-def main():
-  print("face recognition system")
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+​
+import os
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
 
-if __name__=='__main__':
-  main()
+pip install mtcnn
 
+from matplotlib import pyplot as plt
+import cv2
+from PIL import Image
+
+import mtcnn
+from mtcnn.mtcnn import MTCNN
+from matplotlib.patches import Rectangle
+
+from os import listdir
+from tqdm import tqdm
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import seaborn as sns
 
 
 
